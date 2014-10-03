e8ccb7463e35851dcf76c34385f26577743ed3f2
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2014-10-03 10:56:23 -0600

    Changed Employee added social security

--- src/main/java/com/vmware/Employee.java
+++ src/main/java/com/vmware/Employee.java
@@ -1,13 +1,33 @@
 package com.vmware;
 
 public class Employee {
-   private String name;
+	private String name;
+	private String ssn;
 
-   public void setName(String name) {
-      this.name = name;
-   }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-   public String getName() {
-     return name;
-   }
+	public String getName() {
+		return name;
+	}
+
+	public void setSocialSecurityNumber(String ssn) {
+		this.ssn = ssn;
+	}
+
+	public String getSocialSecurityNumber() {
+		return ssn;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
--- src/test/java/com/vmware/EmployeeTest.java
+++ src/test/java/com/vmware/EmployeeTest.java
@@ -4,6 +4,11 @@ import static org.testng.Assert.*;
 
 import org.testng.annotations.Test;
 
+//F2 Brings up the helper
+	//CTRL+F11 Run
+	//CTRL+E Recent Files
+	//F3 Jump to Source
+
 public class EmployeeTest {
 
 	@Test
@@ -13,4 +18,26 @@ public class EmployeeTest {
 		employee.setName(name);
 		assertEquals(employee.getName(), name);
 	}
+	
+	
+	@Test
+	public void addASocialSecurityNumber() {
+		Employee employee = new Employee();
+		employee.setSocialSecurityNumber("123-45-6789");
+		assertEquals(employee.getSocialSecurityNumber(), "123-45-6789");
+	}
+	
+	@Test
+	public void addAnotherSocialSecurityNumber() {
+		Employee employee = new Employee();
+		employee.setSocialSecurityNumber("123-45-6710");
+		assertEquals(employee.getSocialSecurityNumber(), "123-45-6710");
+	}
 }
+
+
+
+
+
+
+
