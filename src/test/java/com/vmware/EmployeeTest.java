9e4d7a854da44a1e024d53030bc9fc363bf42ba2
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2014-10-03 12:51:05 -0600

    refactored Employee to have pattern flexibility

--- src/test/java/com/vmware/EmployeeTest.java
+++ src/test/java/com/vmware/EmployeeTest.java
@@ -2,6 +2,11 @@ package com.vmware;
 
 import static org.testng.Assert.*;
 
+import java.util.regex.Pattern;
+
+import org.junit.Before;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 //F2 Brings up the helper
@@ -11,25 +16,29 @@ import org.testng.annotations.Test;
 
 public class EmployeeTest {
 	
+	private Employee employee;
+	
+	@BeforeMethod
+	public void setUp() {
+	   System.out.println("Construct a new Employee per test");
+	   employee = new Employee(Pattern.compile("\\d{3}-\\d{2}-\\d{4}"));	
+	}
+	
 	@Test
 	public void createEmployeeAndGetName() {
-		Employee employee = new Employee();
 		String name = "Ricardo Montalban";
 		employee.setName(name);
 		assertEquals(employee.getName(), name);
 	}
 	
-	
 	@Test
 	public void addASocialSecurityNumber() {
-		Employee employee = new Employee();
 		employee.setSocialSecurityNumber("123-45-6789");
 		assertEquals(employee.getSocialSecurityNumber(), "123-45-6789");
 	}
 	
 	@Test
 	public void addAnotherSocialSecurityNumber() {
-		Employee employee = new Employee();
 		employee.setSocialSecurityNumber("123-45-6710");
 		assertEquals(employee.getSocialSecurityNumber(), "123-45-6710");
 	}
@@ -37,7 +46,6 @@ public class EmployeeTest {
 	@Test
 	public void fixDE30201_SocialSecurityMustBeACertainFormat() {
 		String badSSN = "RamLikesThe49ers";
-		Employee employee = new Employee();
 		try {
 		   employee.setSocialSecurityNumber(badSSN);
 		   fail("This line should never have been reached");
